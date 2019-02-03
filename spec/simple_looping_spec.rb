0daf9ee3a8968eda6552605f30225c81ce594ebc
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-03 02:22:30 +0000

    Done.

--- spec/simple_looping_spec.rb
+++ spec/simple_looping_spec.rb
@@ -1,5 +1,6 @@
 require_relative './spec_helper'
 
+
 describe 'Loops' do
   before(:each) do
     @looping_string = "Welcome to Flatiron School's Web Development Course!\nWelcome to Flatiron School's Web Development Course!\nWelcome to Flatiron School's Web Development Course!\nWelcome to Flatiron School's Web Development Course!\nWelcome to Flatiron School's Web Development Course!\nWelcome to Flatiron School's Web Development Course!\nWelcome to Flatiron School's Web Development Course!\n"
