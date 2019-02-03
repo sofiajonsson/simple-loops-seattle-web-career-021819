ae3f8cee4a08eca09cc6aa01b3b32f40652b4cb7
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-03 03:53:57 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -24,7 +24,7 @@
 
 def while_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  number_of_times = 1
+  number_of_times = 0
   
 while number_of_times < 7
   puts "#{phrase}"
