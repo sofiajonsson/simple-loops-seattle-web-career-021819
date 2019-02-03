61386ffb54abf16323d2e4c3fadcef471aab745a
Sofia Jonsson <jonssonsofia93@gmail.com>
2019-02-03 04:01:48 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,15 +1,15 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
-phrase = "Welcome to Flatiron School's Web Development Course!"
-number_of_times = 0
+# phrase = "Welcome to Flatiron School's Web Development Course!"
+# number_of_times = 0
   
-loop do 
-  number_of_times += 1
-    puts "#{phrase}"
-    if number_of_times >= 7
-  break
-  end
+# loop do 
+#   number_of_times += 1
+#     puts "#{phrase}"
+#     if number_of_times >= 7
+#   break
+#   end
 end
 
 def times_iterator(number_of_times)
